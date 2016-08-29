a23d79320959231016149cf9c63fecc5abe31d98
mpross <mpross2@gmail.com>
2016-08-29 16:26:05 -0700

    Implemented cross correlation of cuts instead of entire run but ran into indexing errors.

--- Work Google Drive/LIGO/BRS/Earthquake/RWaveArray.m
+++ Work Google Drive/LIGO/BRS/Earthquake/RWaveArray.m
@@ -19,6 +19,8 @@ function [vel, ang, sigmaVel,sigmaAng]=RWaveArray(ETMXZ_out,ETMYZ_out,ITMYZ_out,
     startTime=01*sampf;
     % endTime=800*sampf;
     endTime=length(ETMYZ_out);
+    delta_t_X=[];
+    delta_t_Y=[];
     for i=0:100
         freq2=(startFreq2+i*freqStep2);
     %     [bb,aa] = butter(2,[(0.032+i*0.005)/sampf, (0.032+(i+1)*0.005)/sampf]);
@@ -47,15 +49,19 @@ function [vel, ang, sigmaVel,sigmaAng]=RWaveArray(ETMXZ_out,ETMYZ_out,ITMYZ_out,
     %     X=ETMXZ_out(startTime*sampf:length(ETMXZ_out));
     %     Y=ETMYZ_out(startTime*sampf:length(ETMYZ_out));
     %     C=ITMYZ_out(startTime*sampf:length(ITMYZ_out));
-        if max(C>=threshold)
-            [crossY,~] = xcorr(C,Y);
-            [crossX,lags]=xcorr(C,X);
+    for i=1:floor(length(X)*(freq2/sampf))-2
+        if max(C>=threshold)            
+            i
+            [crossY,~] = xcorr(C(floor(j/(freq2/sampf)):floor((j+1)/(freq2/sampf))...
+                ,Y(floor(j/(freq2/sampf)):floor((j+1)/(freq2/sampf)))));
+            [crossX,lags]=xcorr(C(floor(j/(freq2/sampf)):floor((j+1)/(freq2/sampf))...
+                ,X(floor(j/(freq2/sampf)):floor((j+1)/(freq2/sampf)))));
             crossX=abs(crossX);
             crossY=abs(crossY);
             peak=crossX(floor(.9995*find(crossX==max(crossX))):floor(1.0005*find(crossX==max(crossX))));
             peakLags=lags(floor(.9995*find(crossX==max(crossX))):floor(1.0005*find(crossX==max(crossX))))';
             [fit,s]=polyfit(peakLags,peak,2);  
-            delta_t_X=-fit(2)/(2*fit(1))/sampf;        
+            delta_t_X=[delta_t_X -fit(2)/(2*fit(1))/sampf];        
     %         sigmaTX=std(peak-(fit(1)*peakLags.^2+fit(2)*peakLags+fit(3)));
     %         errFit= sqrt(diag(inv(s.R)*inv(s.R'))./s.normr.^2./s.df);
     %         sigmaTX=sqrt((-1/(2*fit(1))/sampf)^2*errFit(2)^2+(fit(2)/(2*fit(1)^2)/sampf)^2*errFit(1)^2);
@@ -63,26 +69,27 @@ function [vel, ang, sigmaVel,sigmaAng]=RWaveArray(ETMXZ_out,ETMYZ_out,ITMYZ_out,
             peak=crossY(floor(.9995*find(crossY==max(crossY))):floor(1.0005*find(crossY==max(crossY))));
             peakLags=lags(floor(.9995*find(crossY==max(crossY))):floor(1.0005*find(crossY==max(crossY))))';
             [fit,s]=polyfit(peakLags,peak,2);  
-            delta_t_Y=-fit(2)/(2*fit(1))/sampf;       
+            delta_t_Y=[delta_t_Y -fit(2)/(2*fit(1))/sampf];       
     %         sigmaTY=std(peak-(fit(1)*peakLags.^2+fit(2)*peakLags+fit(3)));
     %         errFit= sqrt(diag(inv(s.R)*inv(s.R'))./s.normr.^2./s.df);
     %         sigmaTY=sqrt((-1/(2*fit(1))/sampf)^2*errFit(2)^2+(fit(2)/(2*fit(1)^2)/sampf)^2*errFit(1)^2);
         else
-            delta_t_X=nan;
-            delta_t_Y=nan;
+            delta_t_X=[delta_t_X nan];
+            delta_t_Y=[delta_t_Y nan];
         end
+    end
     %     delta_t_X=(lags(find(crossX==max(crossX))))/sampf;
     %     delta_t_Y=(lags(find(crossY==max(crossY))))/sampf;
     %     delta_t_X=(sum(crossX.*lags)/sum(crossX))/sampf;
     %     delta_t_Y=(sum(crossY.*lags)/sum(crossY))/sampf;
 
-        lagsX=[lagsX delta_t_X];
-        lagsY=[lagsY delta_t_Y];
+%         lagsX=[lagsX delta_t_X];
+%         lagsY=[lagsY delta_t_Y];
 
-        ang=[ang atan2(delta_t_Y,delta_t_X)*180/pi];
-        vel=[vel 4e3/sqrt((delta_t_X)^2+(delta_t_Y)^2)];
-        sigmaAng=[sigmaAng sqrt(delta_t_X^2*sigmaTY^2+delta_t_Y^2*sigmaTX^2)/(delta_t_X^2+delta_t_Y^2)*180/pi];
-        sigmaVel=[sigmaVel 2*4e3*sqrt(delta_t_X^2*sigmaTX^2+delta_t_Y^2*sigmaTY^2)/(delta_t_X^2+delta_t_Y^2)^2];
+        ang=[ang mean(atan2(delta_t_Y,delta_t_X)*180/pi)];
+        vel=[vel mean(4e3/sqrt((delta_t_X).^2+(delta_t_Y).^2))];
+        sigmaAng=[sigmaAng sqrt(delta_t_X.^2.*sigmaTY.^2+delta_t_Y.^2.*sigmaTX.^2)/(delta_t_X.^2+delta_t_Y.^2)*180/pi];
+        sigmaVel=[sigmaVel 2*4e3*sqrt(delta_t_X.^2.*sigmaTX.^2+delta_t_Y.^2.*sigmaTY.^2)/(delta_t_X.^2+delta_t_Y.^2).^2];
     end
     % vel=vel(find(abs(vel-mean(vel))<=abs(3*std(vel))));
 % 
