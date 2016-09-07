aeb61a58b6a585f6d9fda2b4071fb079f87092eb
mpross <mpross2@gmail.com>
2016-09-07 15:43:55 -0700

    Changed the threshold to depend on the RMS value of ETMYZ instead of maximum.

--- Earthquake/RWave.m
+++ Earthquake/RWave.m
@@ -5,21 +5,21 @@ startFreq=0.015;
 freqStep=.005;
 iter=(.1-startFreq)/freqStep;
 [errFreq,transXErr,transYErr,transZErr,tiltErr]=RWaveMeasErr;
-[ETMXZ_out, ITMYZ_out, ETMYX_out, ETMYY_out, ETMYZ_out, BRSY_out]=RWaveDataIn('GPS1143962787_6_9Earthquake.mat');
+[ETMXZ_out, ITMYZ_out, ETMYX_out, ETMYY_out, ETMYZ_out, BRSY_out]=RWaveDataIn('GPS1144888165_7_8Earthquake.mat');
 %     RWaveDataIn('GPS1149323500_Quite.mat');
 % RWaveDataIn('GPS1149331885_6_2Earthquake.mat');% 
-% RWaveDataIn('GPS1144888165_7_8Earthquake.mat');
+% RWaveDataIn('GPS1143962787_6_9Earthquake.mat');
 % RWaveDataIn('GPS1149581095_5_2Earthquake.mat'); 
 bootVel=[];
 bootAng=[];
-threshold=max(abs(ETMYZ_out))/8;
+threshold=rms(ETMYZ_out)/2;
 % seed=randn(1,length(ETMYZ_out));
 
 [vel, ang, sigmaVel,sigmaAng,bootVel,bootAng]=RWaveArray(ETMXZ_out,ETMYZ_out,ITMYZ_out,sampf,threshold,startFreq,freqStep,iter);
 freqStep=0.1/length(ang);
 [v,phi,el,k,sigmaV,sigmaPhi,bootV,bootPhi,bootEl,bootK]=...
 RWaveSingle(ETMYX_out,ETMYY_out,ETMYZ_out,BRSY_out,...
-    'E',errFreq,transXErr,transYErr,transZErr,tiltErr,sampf,ang,threshold,startFreq,freqStep,iter);
+    'S',errFreq,transXErr,transYErr,transZErr,tiltErr,sampf,ang,threshold,startFreq,freqStep,iter);
 
 % figure(1)
 % plot(Rot_time(startTime:length(BRSY_out)),seriesX,Rot_time(startTime:length(BRSY_out))...
