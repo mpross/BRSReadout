b253c5ec9d799bbf94294251a104f5714ca66af9
M.P.Ross <mpross2@uw.edu>
2018-12-10 11:36:16 -0800

    Switched to batch files for install.

---
+++ install_LHOEX.bat
@@ -0,0 +1,3 @@
+copy configs\LHOEX.config BRSReadout\BRSReadout\app.config
+copy configs\LHOEX.config BRSReadout\BRSReadout\bin\Debug\BRSReadout.exe.config
+mklink test BRSReadout\BRSReadout\bin\Debug\BRSReadout.exe
